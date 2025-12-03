41afc3f4618ef6818c2a40211eb1abf0244f64a3
Rami Bitar <rami@frontend.co>
2025-12-03 11:58:35 -0500

    Update About component heading from "About Page" to "About Arch"

--- components/About.tsx
+++ components/About.tsx
@@ -3,8 +3,11 @@ import React from 'react';
 const About: React.FC = () => {
   return (
     <div className="text-center">
-      <h1 className="text-5xl font-bold text-black mb-8" style={{fontFamily: 'Space Grotesk, sans-serif'}}>
-        About Page
+      <h1
+        className="text-5xl font-bold text-black mb-8"
+        style={{ fontFamily: 'Space Grotesk, sans-serif' }}
+      >
+        About Arch
       </h1>
     </div>
   );
